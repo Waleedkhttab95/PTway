161f151b8efbde2719be5ff9b5473c90d74b3122
oneAyman <mcaymann@gmail.com>
2019-03-13 01:01:53 +0300

    finshing waering alert when Charity sector choosen in company sign-up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { AuthData } from './auth-data.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
+declare var $: any;
 
 
 const BackUrl = 'https://cors-anywhere.herokuapp.com/https://ptway-dev.herokuapp.com/api';
@@ -10,7 +12,7 @@ const BackUrl = 'https://cors-anywhere.herokuapp.com/https://ptway-dev.herokuapp
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private isAuth = false;
-  private isCompany : string = "false";
+  private isCompany: string = "false";
   private token: string;
   private userId: string;
   private authStatusListener = new Subject<boolean>();
@@ -35,7 +37,7 @@ export class AuthService {
 
   createCompany(companyName: string, email: string, CompanySpecialist: string, sector: string, password: string) {
     const authData = { companyName: companyName, email: email, CompanySpecialist: CompanySpecialist, sector: sector, password: password };
-    this.http.post<{_id:String}>(BackUrl + '/companyRegistreing', authData,{observe: 'response'})
+    this.http.post<{ _id: String }>(BackUrl + '/companyRegistreing', authData, { observe: 'response' })
       .subscribe(response => {
         console.log(response);
         const token = response.headers.get('x-auth-token');
@@ -44,9 +46,14 @@ export class AuthService {
           this.isAuth = true;
           this.isCompany = "true";
           this.authStatusListener.next(true);
-          this.saveAuthData(token, this.userId,this.isCompany);
-          this.router.navigate(['/add-company-info']);
-
+          this.saveAuthData(token, this.userId, this.isCompany);
+          if (sector == "5c56c3572e168a2c30fe5dde") {
+            console.log('reach');
+            this.showSwal('warning-message');
+          }
+          else {
+            this.router.navigate(['/add-company-info']);
+          }
         }
       }, error => {
         this.authStatusListener.next(false);
@@ -62,7 +69,7 @@ export class AuthService {
         if (token) {
           this.token = token;
           this.isAuth = true;
-          this.isCompany = 'false' ;
+          this.isCompany = 'false';
           this.userId = response.userId;
           this.authStatusListener.next(true);
           this.saveAuthData(token, this.userId, this.isCompany);
@@ -86,7 +93,7 @@ export class AuthService {
           this.isCompany = "true";
           this.userId = response.userId;
           this.authStatusListener.next(true);
-          this.saveAuthData(token, this.userId,this.isCompany);
+          this.saveAuthData(token, this.userId, this.isCompany);
           this.router.navigate(['/dashboard']);
         }
 
@@ -112,10 +119,10 @@ export class AuthService {
     this.router.navigate(['/sign-in']);
   }
 
-  private saveAuthData(token: string, userId: string, isCompany:string) {
+  private saveAuthData(token: string, userId: string, isCompany: string) {
     localStorage.setItem('token', token);
     localStorage.setItem('userId', userId);
-    localStorage.setItem('isCompany',isCompany);
+    localStorage.setItem('isCompany', isCompany);
   }
 
   private clearData() {
@@ -148,5 +155,16 @@ export class AuthService {
       userId: user,
       isCompany: isCompany
     };
+  }
+  showSwal(type) {
+    if (type == 'warning-message') {
+      swal({
+        title: "تمت عملية التسجيل بنجاح!",
+        text: "سيتم ارسال رسالة عبر الريد الالكتروني عند تفعيل الحساب الخاص بك!",
+        buttonsStyling: false,
+        confirmButtonClass: "btn btn-warning",
+        type: "warning"
+      }).catch(swal.noop)
+    }
   }
 }
