c3828b76f5fccf2549138af6353386ae52988b2c
Waleedkhttab95 <Waleedkhttab95>
2019-04-24 09:14:23 +0000

    4/24 from server

--- Backend/index.js
+++ Backend/index.js
@@ -104,7 +104,7 @@ const onListening = () => {
   debug("Listening on " + bind);
 };
 
-const port = normalizePort(process.env.PORT || "80");
+const port = normalizePort(process.env.PORT || "5000");
 app.set("port", port);
 
 const server = http.createServer(app);
