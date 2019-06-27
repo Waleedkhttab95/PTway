82a5a194f30921b06ba1e07e97f408398233c35a
ptway <waleed.khattab@ptway.net>
2019-06-27 12:38:10 +0300

    6/27 3 from local

--- src/app/add-user-info/add-user-info.component.ts
+++ src/app/add-user-info/add-user-info.component.ts
@@ -289,12 +289,12 @@ export class AddUserInfoComponent implements OnInit {
 
   postuserinfo() {
   
-    if(this.userResumeForm.get('fullName').invalid || this.userResumeForm.get('country').invalid
-    || this.userResumeForm.get('city').invalid || this.userResumeForm.get('gender').invalid ||
-    this.userResumeForm.get('public_Major').invalid || this.userResumeForm.get('birthDate').invalid) {
+    // if(this.userResumeForm.get('fullName').invalid || this.userResumeForm.get('country').invalid
+    // || this.userResumeForm.get('city').invalid || this.userResumeForm.get('gender').invalid ||
+    // this.userResumeForm.get('public_Major').invalid || this.userResumeForm.get('birthDate').invalid) {
     
-      return;
-    }
+    //   return;
+    // }
   
  
 console.log(this.userResumeForm.value)
