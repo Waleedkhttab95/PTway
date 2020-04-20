8e92f2ab0b0d027c3758e51303cf7fdcf857b29a
ptway <waleed.khattab@ptway.net>
2020-04-20 23:26:15 +0300

    fix bigs

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28104315185626e9760d0863eff7cee4.js"
+  "/precache-manifest.a07b6314b81598c45d86d58923dfe0f1.js"
 );
 
 self.addEventListener('message', (event) => {
