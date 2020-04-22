bb65025230a2f2ea2546e6fec93938ca6a8be335
ptway <waleed.khattab@ptway.net>
2020-04-23 02:37:43 +0300

    edit form temp

--- routes/temp.js
+++ routes/temp.js
@@ -8,28 +8,35 @@ module.exports = (app) =>{
 
 
  app.post('/api/postTempForm',async (req,res) =>{
-   new formTemp({
-      name : req.body.name,
-      birthDate : req.body.birthDate,
-      gender: req.body.gender,
-      isSaudi: req.body.isSaudi,
-      company:req.body.company,
-      nationality: req.body.nationality,
-      city:req.body.city, 
-      street: req.body.street,
-      mobile: req.body.mobile,
-      email: req.body.email,
-      avilableCar:req.body.avilableCar,
-      carType:req.body.carType,
-      carModel:req.body.carModel,
-      jobTitle:req.body.jobTitle,
-      timeToDelivier:req.body.timeToDelivier,
-      mobileOS: req.body.mobileOS,
-      exp: req.body.exp,
-      social: req.body.social,
-      ptwayMember:req.body.ptwayMember
-   }).save().then(result => { 
-    res.send(result); })
+    const user = await formTemp.findOne({'email': req.body.email})
+    if(!user){
+      new formTemp({
+         name : req.body.name,
+         birthDate : req.body.birthDate,
+         gender: req.body.gender,
+         isSaudi: req.body.isSaudi,
+         company:req.body.company,
+         nationality: req.body.nationality,
+         city:req.body.city, 
+         street: req.body.street,
+         mobile: req.body.mobile,
+         email: req.body.email,
+         avilableCar:req.body.avilableCar,
+         carType:req.body.carType,
+         carModel:req.body.carModel,
+         jobTitle:req.body.jobTitle,
+         timeToDelivier:req.body.timeToDelivier,
+         mobileOS: req.body.mobileOS,
+         exp: req.body.exp,
+         social: req.body.social,
+         ptwayMember:req.body.ptwayMember
+      }).save().then(result => { 
+       res.send(result); })
+    }
+    else{
+       res.status(200).send('Done .')
+    }
+
  })
 
 
