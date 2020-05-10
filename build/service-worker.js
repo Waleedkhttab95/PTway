f7e9939461381b8a495baf093e26c7b97702b1dc
ptway <waleed.khattab@ptway.net>
2020-05-10 05:56:06 +0300

    f

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c145f9be789f7c6d8bc5d2772e7555d.js"
+  "/precache-manifest.47df943ac3b87a553bce01995e43ba13.js"
 );
 
 self.addEventListener('message', (event) => {
